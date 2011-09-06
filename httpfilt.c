e9ef4c21b3f0317ca6a915d0700cd0bc41f2a3a5
Theo Schlossnagle <jesus@omniti.com>
2011-09-06 11:48:41 -0400

    replace case/memcmp with a switch - from Dan McDonald

--- httpfilt.c
+++ httpfilt.c
@@ -56,7 +56,24 @@ typedef struct httpf {
 	int		httpf_method;	/* HTTP method */
 	int		httpf_sm;	/* \r\n\r\n | \n\n state machine */
 	char		httpf_ff[4];	/* the first 4 bytes of the data stream */
+	union {
+		char httpfu_ff[4]; /* the first 4 bytes of the data stream */
+		uint32_t httpfu_w; /* in an easy-to-compare 32-bit word */
+	} httpf_u;
+#define httpf_ff httpf_u.httpfu_ff
+#define httpf_ffw httpf_u.httpfu_w
 } httpf_t;
+
+#ifdef _BIG_ENDIAN
+#define CHAR_TO_32(a, b, c, d) ((d) + ((c) << 8) + ((b) << 16) + ((a) << 24))
+#else
+#define CHAR_TO_32(a, b, c, d) ((a) + ((b) << 8) + ((c) << 16) + ((d) << 24))
+#endif
+
+#define WORD_GET        CHAR_TO_32('G', 'E', 'T', ' ')
+#define WORD_PUT        CHAR_TO_32('P', 'U', 'T', ' ')
+#define WORD_POST       CHAR_TO_32('P', 'O', 'S', 'T')
+#define WORD_HEAD       CHAR_TO_32('H', 'E', 'A', 'D')
 
 #define HTTPF_METHOD_INVALID -1
 #define HTTPF_METHOD_UNSET 0
@@ -79,74 +96,21 @@ httpf_method_name(int m) {
 }
 
 static int
-httpf_method_from_ff(const char *ff) {
-	switch(*ff) {
-	case 'G':
-		if(memcmp(ff, "GET ", 4) == 0)
-			return HTTPF_METHOD_GET;
-		break;
-	case 'H':
-		if(memcmp(ff, "HEAD", 4) == 0)
-			return HTTPF_METHOD_HEAD;
-		break;
-	case 'P':
-		if(memcmp(ff, "POST", 4) == 0)
-			return HTTPF_METHOD_POST;
-		else if(memcmp(ff, "PUT ", 4) == 0)
-			return HTTPF_METHOD_PUT;
-		break;
+httpf_method_from_ff(uint32_t ffw)
+{
+	switch (ffw) {
+	case WORD_GET:
+	case WORD_GET_TAB:
+		return (HTTPF_METHOD_GET);
+	case WORD_PUT:
+		return (HTTPF_METHOD_PUT);
+	case WORD_POST:
+		return (HTTPF_METHOD_POST);
+	case WORD_HEAD:
+		return (HTTPF_METHOD_HEAD);
 	}
-	return HTTPF_METHOD_INVALID;
-}
 
-static int
-httpf_process_input(httpf_t *httpf, mblk_t *mp) {
-	int i, dlen = MBLKL(mp);
-	for(i=0; i<dlen; i++) {
-		/* Collect the first four bytes for a protocol validation */
-		if(httpf->httpf_method == HTTPF_METHOD_UNSET &&
-		    httpf->httpf_bytes_in < 4)
-			httpf->httpf_ff[httpf->httpf_bytes_in] = mp->b_rptr[i];
-
-		httpf->httpf_bytes_in++;
-
-		/* if we haven't yet determined out HTTP method, do it at
-                   exactly 4 bytes into the stream. */
-		if(httpf->httpf_method == HTTPF_METHOD_UNSET &&
-		    httpf->httpf_bytes_in == 4) {
-			/* if we find no good method, we can't defer this stream */
-			httpf->httpf_method = httpf_method_from_ff(httpf->httpf_ff);
-			if(httpf->httpf_method == HTTPF_METHOD_INVALID)
-				return -1;
-		}
-
-		/* if the method is set, start looking for either \r\n\r\n or \n\n */
-#define HTTPF_STATE(a) httpf->httpf_sm = (a)
-#define IF_HTTPF_TOKEN(a) if (mp->b_rptr[i] == (a))
-		if(httpf->httpf_method > HTTPF_METHOD_UNSET) {
-			switch(httpf->httpf_sm) {
-				case 0:
-					IF_HTTPF_TOKEN('\r') HTTPF_STATE(1);
-					IF_HTTPF_TOKEN('\n') HTTPF_STATE(3);
-					break;
-				case 1:
-					IF_HTTPF_TOKEN('\n') HTTPF_STATE(2);
-					else HTTPF_STATE(0);
-					break;
-				case 2:
-					IF_HTTPF_TOKEN('\n') return 1;
-					IF_HTTPF_TOKEN('\r') HTTPF_STATE(3);
-					else HTTPF_STATE(0);
-					break;
-				case 3:
-					IF_HTTPF_TOKEN('\n') return 1;
-					IF_HTTPF_TOKEN('\r') HTTPF_STATE(1);
-					else HTTPF_STATE(0);
-					break;
-			}
-		}
-	}
-	return 0;
+	return (HTTPF_METHOD_INVALID);
 }
 
 /*
@@ -167,12 +131,12 @@ httpf_attach_passive_cb(sof_handle_t handle, sof_handle_t ph,
 		return (SOF_RVAL_ENOMEM);
 
 	new->httpf_bytes_in = 0;
-	new->httpf_method = 0;
+	new->httpf_method = HTTPF_METHOD_UNSET;
 
 	*cookiep = new;
 	/*
 	 * We are awaiting a request, defer the notification of this
-         * connection until it is completed.
+	 * connection until it is completed.
 	 */
 	return (SOF_RVAL_DEFER);
 }
@@ -241,12 +205,12 @@ httpf_data_in_cb(sof_handle_t handle, void *cookie, mblk_t *mp, int flags,
 		return (mp);
 	}
 
-        if (mp == NULL) return (mp);
+	if (mp == NULL) return (mp);
 
 	if(httpf_process_input(httpf, mp))
 		sof_newconn_ready(handle);
 
-        if(httpf->httpf_bytes_in > MAX_HTTP_FILTER_SIZE)
+	if(httpf->httpf_bytes_in > MAX_HTTP_FILTER_SIZE)
 		sof_newconn_ready(handle);
 
 	return mp;
@@ -263,30 +227,29 @@ sof_ops_t httpf_ops = {
 int
 _init(void)
 {
-        int error;
+	int error;
 
-        if ((error = sof_register(SOF_VERSION, HTTPFILT_MODULE,
-            &httpf_ops, 0)) != 0)
-                return (error);
-        if ((error = mod_install(&httpf_modlinkage)) != 0)
-                (void) sof_unregister(HTTPFILT_MODULE);
+	if ((error = sof_register(SOF_VERSION, HTTPFILT_MODULE, &httpf_ops, 0)) != 0)
+		return (error);
+	if ((error = mod_install(&httpf_modlinkage)) != 0)
+		(void) sof_unregister(HTTPFILT_MODULE);
 
-        return (error);
+	return (error);
 }
 
 int
 _fini(void)
 {
-        int error;
+	int error;
 
-        if ((error = sof_unregister(HTTPFILT_MODULE)) != 0)
-                return (error);
+	if ((error = sof_unregister(HTTPFILT_MODULE)) != 0)
+		return (error);
 
-        return (mod_remove(&httpf_modlinkage));
+	return (mod_remove(&httpf_modlinkage));
 }
 
 int
 _info(struct modinfo *modinfop)
 {
-        return (mod_info(&httpf_modlinkage, modinfop));
+	return (mod_info(&httpf_modlinkage, modinfop));
 }
